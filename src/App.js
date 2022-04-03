147b6dcf6ca246e59d212e04dab7748ec45f946a
Itamar Bachar <itamarb25@gmail.com>
2022-04-03 18:58:47 +0300

    chrome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import { useNavigate } from "react-router-dom";
 import Contact from './ChatPage/Contact';
+import { useHistory } from "react-router-dom";
 import React, { Component } from "react";
 import './AppStyle.css';
 import { BrowserRouter, Routes, Route, Link, Router } from 'react-router-dom';
@@ -9,15 +11,26 @@ import Chat from "./ChatPage/Chat";
 
 //if i want to write children inside my component, instead of returning it in them, i can write them between its tag and make sure to return props.children from it.
 class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      flag: true
+    }
+    this.handleMovePages = this.handleMovePages.bind(this)
+  }
+
+
   render() {
     return (
       <div>
         <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login/>}></Route>
-          <Route path='Register' element={<Register/>}></Route>
-          <Route path='./ChatPage/Chat' element={<Chat/>}></Route>
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Login handleMovePages={this.handleMovePages} />}></Route>
+            <Route path='Register' element={<Register />}></Route>
+            <Route path='./ChatPage/Chat' element={<Chat />}></Route>
+          </Routes>
         </BrowserRouter>
       </div>
     )
--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -1,12 +1,47 @@
-
+import { useHistory } from "react-router-dom";
 import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router';
 import React, { Component } from "react";
 import './Login.css';
+let contactList = [
+    { username: "Itamar", Password: 1234 },
+    { username: "Coral", Password: 1234 },
+    { username: "Amit", Password: 1234 }
+]
+
 class Login extends Component {
     constructor(props) {
         super(props);
-        // this.handleClick = this.handleClick.bind(this);
         this.state = {
+            userName: '',
+            password: ''
+        }
+        this.handleChangeUserName = this.handleChangeUserName.bind(this);
+        this.handleChangePassword = this.handleChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    handleChangeUserName(event) {
+        this.setState({ userName: event.target.value, password: this.state.password });
+    }
+    handleChangePassword(event) {
+        this.setState({ userName: this.state.userName, password: event.target.value });
+    }
+
+    onSubmit(event) {
+        var user;
+        for (var i = 0; i < contactList.length; i++) {
+            if (this.state.userName === contactList[i].username && this.state.password == contactList[i].Password) {
+                user = contactList[i]
+                break;
+            }
+        }
+        if (user != null) {
+            return (<Navigate to={{
+                pathname: '/order',
+                state: { id: '123' }
+            }} />);
+        } else {
+            return (<div>Login Please</div>);
         }
     }
 
@@ -15,21 +50,21 @@ class Login extends Component {
         return (
             <div>
                 <h1 id="uppart"></h1>
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className="row mb-3 form">
-                        <label className="col-sm-2 col-form-label">Username</label>
+                        <label className="col-sm-2 col-form-label" >Username</label>
                         <div className="col-sm-4">
-                            <input id='username' className="form-control"></input>
+                            <input id='username' className="form-control" onChange={this.handleChangeUserName}></input>
                         </div>
                     </div>
-                    <div className="row mb-3 form">
+                    <div className="row mb-3 form" >
                         <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                         <div className="col-sm-4">
-                            <input type="password" className="form-control" id="inputPassword3"></input>
+                            <input type="password" className="form-control" id="inputPassword3" onChange={this.handleChangePassword}></input>
                         </div>
                     </div>
                     <div className='signButton'>
-                        <button type="submit" className="btn btn-primary" onClick={this.handleClick}>Login</button>
+                        <button type="submit" className="btn btn-primary">Login</button>
                         <span className='register'>Not registered? </span>
                         <Link to='/Register'>Click here</Link>
                         <span> to register.</span>
