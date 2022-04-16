836660709c5353fad8b589a090625c97580d3e66
matanshamir1998 <matan.shamir@live.biu.ac.il>
2022-04-16 15:49:30 +0300

    added oclock

--- src/ChatPage/Contact.js
+++ src/ChatPage/Contact.js
@@ -13,17 +13,19 @@ class Contact extends Component {
     }
 
     render() {
+        let message = this.props.message.length<30 ? this.props.message : this.props.message.substring(0,28)+"..."
         return (
             <li className={this.props.styles} onClick={this.changeState}>
                 <div className="ms-2 me-auto">
                     {this.props.news !== 0 && (<span className="badge bg-primary rounded-pill to-right">{this.props.news}</span>)}
+                    {this.props.oclock !== '' && (<span className="oclock">{this.props.oclock}</span>)}
                     <div className="position-relative avatar cont-div">
                         <img src={this.props.source} alt="Avatar" className="circular-square"></img>
                     </div>
                     <div className="fw-bold">
                         {this.props.name}
                     </div>
-                    {this.props.message}
+                    {message}
                 </div>
             </li> //do that the onclick will set this to true. it is essential to use () => because the function should know what is the component, "this".
         )
