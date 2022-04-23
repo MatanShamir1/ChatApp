55dfdbe33a5f71b92ecfa32f1f0397c6cbc83a71
matanshamir1998 <matan.shamir@live.biu.ac.il>
2022-04-23 23:02:20 +0300

    added hard-coded essential messages

--- src/ChatPage/contactLists.js
+++ src/ChatPage/contactLists.js
@@ -4,20 +4,21 @@ import imgCatelyn from '../images/catelyn_stark.png'
 import imgNed from '../images/Ned_Stark.jpg'
 import imgRob from '../images/rob_stark.jpg'
 import imgSansa from '../images/sansa_stark.jpg'
-
+import record from '../images/welcome_record.ogg'
+import video from '../images/example_video.mp4'
 
 let contactLists = [
-    ["054-1234567", [{ name: "Sansa", phoneNumber: "054-3456789", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgSansa },  //jon
-    { name: "Arya", phoneNumber: "054-2345678", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgArya },
+    ["054-1234567", [{ name: "Sansa", phoneNumber: "054-3456789", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"],[record, "record", "snd", "08:41, 04/14/2022"]], new: 1, source: imgSansa },  //jon
+    { name: "Arya", phoneNumber: "054-2345678", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"], [imgNed, "image", "snd", "08:41, 04/14/2022"], [video, "video", "rcv", "08:42, 04/14/2022"]], new: 1, source: imgArya },
     { name: "Rob", phoneNumber: "054-4567890", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgRob },
     { name: "Eddard", phoneNumber: "054-6789012", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgNed },
     { name: "Catelyn", phoneNumber: "054-5678901", messages: [['what is up?',"text","rcv","08:40, 04/14/2022"]] , new: 1, source: imgCatelyn }]],
     ["054-2345678", [{ name: "Sansa", phoneNumber: "054-3456789", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgSansa },  //arya
-    { name: "Jon", phoneNumber: "054-1234567", messages: [['what is up?', "text", "snd", "08:40, 04/14/2022"]], new: 0, source: imgJon },
+    { name: "Jon", phoneNumber: "054-1234567", messages: [['what is up?', "text", "snd", "08:40, 04/14/2022"], [imgNed, "image", "rcv", "08:41, 04/14/2022"] [video, "video", "snd", "08:42, 04/14/2022"]], new: 0, source: imgJon },
     { name: "Rob", phoneNumber: "054-4567890", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgRob },
     { name: "Eddard", phoneNumber: "054-6789012", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgNed },
     { name: "Catelyn", phoneNumber: "054-5678901", messages: [['what is up?',"text","rcv","08:40, 04/14/2022"]] , new: 1, source: imgCatelyn }]],
-    ["054-3456789", [{ name: "Jon", phoneNumber: "054-1234567", messages: [['what is up?', "text", "snd", "08:40, 04/14/2022"]], new: 0, source: imgJon },  //sansa
+    ["054-3456789", [{ name: "Jon", phoneNumber: "054-1234567", messages: [['what is up?', "text", "snd", "08:40, 04/14/2022"], [record, "record", "rcv", "08:41, 04/14/2022"]], new: 0, source: imgJon },  //sansa
     { name: "Arya", phoneNumber: "054-2345678", messages: [['what is up?', "text", "snd", "08:40, 04/14/2022"]], new: 0, source: imgArya },
     { name: "Rob", phoneNumber: "054-4567890", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgRob },
     { name: "Eddard", phoneNumber: "054-6789012", messages: [['what is up?', "text", "rcv", "08:40, 04/14/2022"]], new: 1, source: imgNed },
